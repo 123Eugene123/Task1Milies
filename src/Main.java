909c4e9cba919d873f1d0e2e53f327dc8d1f02c6
Eugene test <eupushkash@ngs.ru>
2023-08-17 19:54:35 +0300

    Commit2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,8 @@
-import java.sql.SQLOutput;
-
 public class Main {
     public static void main(String[] args) {
         int price = 21_200;
         int discount = 20;
         int miles = (price / discount);
         System.out.println("Милли = " + miles);
-            }
-
+    }
 }
